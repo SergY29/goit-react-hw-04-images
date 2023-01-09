6fcb63874b631bd66cc14e51cf3023638aba3357
SergY29 <yurtin.sergey.v@gmail.com>
2023-01-09 15:13:04 +0200

    start

---
+++ src/components/services/services.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+import axios from 'axios';
+
+export const getImages = async (nextRequest, prevPage) => {
+  const { data } = await axios.get('https://pixabay.com/api/', {
+    params: {
+      q: nextRequest,
+      page: prevPage,
+      key: '31232052-ebca7977e423ff0aad3113109',
+      image_type: 'photo',
+      orientation: 'horizontal',
+      per_page: 12,
+    },
+  });
+
+  return data;
+};
+
+getImages.propTypes = {
+  nextRequest: PropTypes.string.isRequired,
+  prevPage: PropTypes.number.isRequired,
+};
